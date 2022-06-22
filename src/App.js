b833363022287408e3c20d54b0b3582ccdbd2fa5
Hernan Funes <funes.hernan@hotmail.es>
2022-06-21 21:59:31 -0300

    pruebas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import {BrowserRouter,Route, Routes } from 'react-router-dom';
-import './App.css';
+//Libraries
+import {Route, Routes } from 'react-router-dom';
+//Components
 import Login from './components/Login';
 import Listado from './components/Listado';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+//Styles
+import './css/app.css';
 
 function App() {
   return (
     <>
 
-      
+      <Header/>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/listado' element={<Listado/>}/>
       </Routes>
+      <Footer/>
       
 
     </>
